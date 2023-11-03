409c538827a75ebe3adf9ab64360a55e1b9df272
Ruth <ruro122020@gmail.com>
2023-11-03 17:19:44 -0400

    search by title functionality

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -3,75 +3,89 @@ import { useOutletContext } from 'react-router-dom'
 import MovieCard from '../components/MovieCard'
 
 const Movies = () => {
-  const[sortBy, setSortBy] = useState('')
-  const [titleSearch, setTitle] = useState('')
+  const [sortBy, setSortBy] = useState('')
+  const [titleSearch, setTitleSearch] = useState('')
   const [genreSearch, setGenreSearch] = useState('')
   const movies = useOutletContext()
 
-  const handleSort =(e)=>{
+  const handleSort = (e) => {
     setSortBy(e.target.value)
   }
+  const handleSearch = (e) => {
+    if (e.target.name === 'title') {
+      setTitleSearch(e.target.value)
+    } else {
+      setGenreSearch(e.target.value)
+    }
+  }
 
-  const filteredMovies =()=>{
+  const filteredMovies = () => {
     const copyMovies = [...movies]
-    if(sortBy === '' && titleSearch === '' && genreSearch === '') {
-      return movies
-    }
-
-    if(sortBy === 'Alphabet'){
-      //sort movies in alphabetical order
-      copyMovies.sort((movieA, movieB) => {
-        const nameA = movieA.title.toUpperCase();
-        const nameB = movieB.title.toUpperCase(); 
-        if (nameA < nameB) {
-          return -1;
+    return copyMovies.filter(movie => {
+      if(titleSearch === '') return true
+      return movie.title.toLowerCase().includes(titleSearch.toLowerCase())
+    })
+    .sort((movieA, movieB) => {
+        if(sortBy === 'Alphabet'){
+          const nameA = movieA.title.toUpperCase();
+          const nameB = movieB.title.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+          return 0;
+        }else if(sortBy === 'Rating'){
+          return movieB.selectRating - movieA.selectRating
         }
-        if (nameA > nameB) {
-          return 1;
-        }
-        return 0;
-      });
-      return copyMovies
-    }
-
-    if(sortBy === 'Rating'){
-      //sort movies by rating greatest to least
-      copyMovies.sort((movieA, movieB) =>movieB.selectRating - movieA.selectRating );
-      return copyMovies
-    }
+      })
   }
 
   const formatMovies = filteredMovies()
 
-  const displayMovies = formatMovies.map(movie => <MovieCard key={movie.id} title={movie.title} id={movie.id} rating={movie.selectRating}/>)
+  const displayMovies = formatMovies.map(movie => <MovieCard key={movie.id} title={movie.title} id={movie.id} rating={movie.selectRating} />)
 
   return (
     <div>
-      <input type='search' placeholder='Search Title...' />
-      <input type='search' placeholder='Search Genre...' />
-      <div>
-        <strong>Sort By</strong>
-        <label>
-          <input 
-          type="radio" 
-          name="sort" 
-          value="Alphabet" 
-          checked={sortBy==='Alphabet'} 
-          onChange={handleSort}/>
-          Alphabet
-        </label>
-        <label>
-          <input 
-          type="radio" 
-          name="sort" 
-          value="Rating" 
-          checked={sortBy === 'Rating'}
-          onChange={handleSort}
-          />
-          Rating 
-        </label>
+      <form>
+        <input
+          type='search'
+          placeholder='Search Title...'
+          name='title'
+          value={titleSearch}
+          onChange={handleSearch} />
+        <input
+          type='search'
+          placeholder='Search Genre...'
+          name='genre'
+          value={genreSearch}
+          onChange={handleSearch}
+        />
+        <div>
+          <strong>Sort By</strong>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              value="Alphabet"
+              checked={sortBy === 'Alphabet'}
+              onChange={handleSort} />
+            Alphabet
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              value="Rating"
+              checked={sortBy === 'Rating'}
+              onChange={handleSort}
+            />
+            Rating
+          </label>
 
-      </div>
+        </div>
+      </form>
       <h1>Movies</h1>
       {displayMovies}
     </div>
