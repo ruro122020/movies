23f7160dc93988111acfd3cd74df5cb5a44efe48
Ruth <ruro122020@gmail.com>
2023-11-02 20:18:51 -0400

    fetch movies from server in App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState,useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
-
 function App() {
+  const [movies, setMovies] = useState([])
+  useEffect(()=>{
+    fetch(`http://localhost:3001/movies`)
+    .then(res=> res.json())
+    .then(movies => setMovies(movies))
+  }, [])
+ 
   return (
     <div >
       <header>
         <NavBar />
       </header>
-      <Outlet />
+      <Outlet context={movies} />
     </div>
   );
 }
